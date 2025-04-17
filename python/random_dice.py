ce0d8bb7d8d6fec8febdcabf2ef52f04ee434c4e
Adios159 <83947563+Adios159@users.noreply.github.com>
2025-04-17 06:51:34 +0000

    4개로 수정

--- python/random_dice.py
+++ python/random_dice.py
@@ -1,15 +1,16 @@
 import random
 
 count = 0
-dice1, dice2, dice3 = 0, 0, 0,
+dice1, dice2, dice3, dice4 = 0, 0, 0, 0
 
 while True:
     count += 1
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
     dice3 = random.randint(1,6)
+    dice4 = random.randint(1,6)
 
-    if (dice1 == dice2) and (dice2 == dice3):
+    if (dice1 == dice2) and (dice2 == dice3) and (dice3 == dice4):
         break
 
 print("3개 주사위 모두", dice1, "이 나왔습니다")
