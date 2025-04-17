d39347205ec202db83bcebcd7d2f8008d06ba7b5
Adios159 <83947563+Adios159@users.noreply.github.com>
2025-04-17 05:00:55 +0000

    while 추가

--- python/RPS.py
+++ python/RPS.py
@@ -1,31 +1,35 @@
 import random
 
-myhand = input("나의 가위/바위/보 ==> ")
+while True:
+    myhand = input("나의 가위/바위/보 ==> ")
 
-comhand = random.choice(["가위", "바위", "보"])
-print("컴퓨터의 가위/바위/보 ==> ", comhand)
+    comhand = random.choice(["가위", "바위", "보"])
+    print("컴퓨터의 가위/바위/보 ==> ", comhand)
 
-if myhand == "가위":
-    if comhand == "가위":
-        print("비겼습니다. ㅡ.ㅡ")
-    elif comhand == "바위":
-        print("졌습니다. ㅠㅠ")
-    elif comhand == "보":
-        print("이겼습니다. ^^")
+    if myhand == "가위":
+        if comhand == "가위":
+            print("비겼습니다. ㅡ.ㅡ")
+        elif comhand == "바위":
+            print("졌습니다. ㅠㅠ")
+        elif comhand == "보":
+            print("이겼습니다. ^^")
 
-elif myhand == "바위":
-    if comhand == "가위":
-        print("이겼습니다. ^^")
-    elif comhand == "바위":
-        print("비겼습니다. ㅡ.ㅡ")
-    elif comhand == "보":
-        print("졌습니다. ㅠㅠ")
+    elif myhand == "바위":
+        if comhand == "가위":
+            print("이겼습니다. ^^")
+        elif comhand == "바위":
+            print("비겼습니다. ㅡ.ㅡ")
+        elif comhand == "보":
+            print("졌습니다. ㅠㅠ")
 
-elif myhand == "보":
-    if comhand == "가위":
-        print("졌습니다. ㅠㅠ")
-    elif comhand == "바위":
-        print("이겼습니다. ^^")
-    elif comhand == "보":
-        print("비겼습니다. ㅡ.ㅡ")
+    elif myhand == "보":
+        if comhand == "가위":
+            print("졌습니다. ㅠㅠ")
+        elif comhand == "바위":
+            print("이겼습니다. ^^")
+        elif comhand == "보":
+            print("비겼습니다. ㅡ.ㅡ")
 
+    elif myhand == "끝내기" :
+        print("게임을 종료합니다")
+        break
